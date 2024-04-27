a54ff2f2f50f43f8c43688fe757d33e598ea246e
Jake Dusing <dusingjake@gmail.com>
2024-04-27 15:19:19 -0500

    add handling for control and command key

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,13 +3,21 @@ import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
+
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
