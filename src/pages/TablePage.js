2bcde6e508ed9b0ccedbf76474be28e5a498582a
Jake Dusing <dusingjake@gmail.com>
2024-05-19 18:40:15 -0500

    build rows for table

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,16 @@
 import Table from "../components/Table";
 
 function TablePage() {
+  const data = [
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Red", color: "bg-red-500", score: 4 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 2 },
+  ];
+
   return (
     <div>
-      <Table />
+      <Table data={data} />
     </div>
   );
 }
