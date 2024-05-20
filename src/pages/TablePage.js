a2845f1af7ac377509093d9693ffe5cf2d2695db
Jake Dusing <dusingjake@gmail.com>
2024-05-19 19:59:53 -0500

    update table styling

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -3,7 +3,7 @@ import Table from "../components/Table";
 function TablePage() {
   const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
-    { name: "Red", color: "bg-red-500", score: 4 },
+    { name: "Apple", color: "bg-red-500", score: 4 },
     { name: "Banana", color: "bg-yellow-500", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 2 },
   ];
