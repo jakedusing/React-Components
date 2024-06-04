a5ad0591b22e97ea344ca572d36d275b89bf5534
Jake Dusing <dusingjake@gmail.com>
2024-06-04 07:32:17 -0500

    add new state updates

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -4,6 +4,8 @@ import Panel from "../components/Panel";
 
 const INCREMENT_COUNT = "increment";
 const SET_VALUE_TO_ADD = "set-value-to-add";
+const DECREMENT_COUNT = "decrement";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -11,6 +13,17 @@ const reducer = (state, action) => {
       return {
         ...state,
         count: state.count + 1,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
       };
     case SET_VALUE_TO_ADD:
       return {
@@ -37,7 +50,9 @@ function CounterPage({ initialCount }) {
   };
 
   const decrement = () => {
-    //  setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
 
   const handleChange = (event) => {
@@ -52,8 +67,9 @@ function CounterPage({ initialCount }) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    //  setCount(count + valueToAdd);
-    //  setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
 
   return (
